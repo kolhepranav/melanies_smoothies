f9532ae3397ff0119f18b8465cf345def78a7d03
Pranav Kolhe <143176974+kolhepranav@users.noreply.github.com>
2026-02-13 19:34:45 +0530

    Fix connection setup for Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 
-cnx = st.connection("my_connection",type="snowflake")
-# cnx = st.connection("snowflake")
+# cnx = st.connection("my_connection",type="snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
